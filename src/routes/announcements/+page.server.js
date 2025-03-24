27de9edefa7ccb70f5bdb030c81e7dbf6b7b8431
Aman Bhargava <amanbhargava2001@gmail.com>
2025-03-24 19:14:06 +0530

    remove announcements

--- src/routes/announcements/+page.server.js
+++
@@ -1,9 +0,0 @@
-import { getAnnouncements } from '$lib/utils/announcements';
-
-export async function load() {
-  const announcements = await getAnnouncements();
-  
-  return {
-    announcements
-  };
-} 
